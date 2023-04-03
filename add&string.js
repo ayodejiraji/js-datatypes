b2b5c2e69c05e33fdd43a30e75a612494c3d1c4b
kennyboy12 <rkehinde774@gmail.com>
2023-04-03 11:40:40 +0100

    first commit- committing all files to the repository

---
+++ add&string.js
@@ -0,0 +1,56 @@
+let x=16 + "Volvo"; 
+console.log(x);
+
+let y="toyota"+6
+console.log(y);
+
+let a="benz"-"7";
+console.log(a);
+
+let b="Beetle"+"8";
+console.log(b);
+
+let c=5-4+"beetle";
+console.log(c);
+
+let d="benz"-7+6;
+console.log(d);
+
+let e="toyota"+3+3;
+console.log(e);
+
+let f=6+9+"beetle";
+console.log(f);
+
+let g;           // now g is undefined
+  g=6;          // now g is a number
+  g="kenny";    // now g is a string
+  console.log(g);
+
+  // Using double quotes:
+let carName1 = "benz XC60";
+console.log(carName1);
+
+// Using single quotes:
+let carName2 = 'Volvo XC60';
+console.log(carName2);
+
+// Single quote inside double quotes:
+let answer1 = "It's alright";
+console.log(answer1)
+
+// Single quotes inside double quotes:
+let answer2 = "He is called 'Johnny'";
+console.log(answer2);
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+console.log(answer3);
+
+// With decimals:
+let x1 = 34.00;
+console.log(x1);
+
+// Without decimals:
+let x2 = 34; 
+console.log(x2);
---
+++ me.js
@@ -0,0 +1,20 @@
+let x=(8/2)**2;
+console.log(x);
+
+let y=8+2;
+console.log(y);
+
+let z=8-2;
+console.log(z);
+
+let a=8*2;
+console.log(a);
+
+let b=8/2;
+console.log(b);
+
+let c=8+2*5/2;
+console.log(c);
+
+let d=(13%5)**2;
+console.log(d);
